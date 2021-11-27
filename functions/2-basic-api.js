efa048587b12eee74d3467c1d730286dd6a1fed9
Santo120199 <santo120199@gmail.com>
2021-11-27 16:47:52 +0100

    complete

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -1,8 +1,10 @@
 const items = require('../assets/data')
 
 exports.handler = async (event, context) => {
-  console.log(context)
   return {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   }
